115979d558285e87684e94d6f6725c4af64e25ce
sagarikaghom14 <sagarika.ghom@mitaoe.ac.in>
2024-10-16 09:15:38 +0530

    added new project

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const isloggedin = require("../middlewares/isLoggedIn");
+const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
+
+router.get("/", function (req, res) {
+  let error = req.flash("error");
+  res.render("index", { error, loggedin: false });
+});
+
+router.get("/shop", isloggedin, async function (req, res) {
+  let products = await productModel.find();
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+
+router.get("/addtocart/:id", isloggedin, async function (req, res) {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.id);
+  await user.save();
+  req.flash("success", "Added to cart");
+  res.redirect("/shop");
+});
+
+router.get("/cart", isloggedin, async function (req, res) {
+  let user = await userModel.findOne({ email: req.user.email }).populate("cart");
+
+  const bill =Number(user.item.price) + 20 - Number(user.item.discount);
+  res.render("cart", { user , bill});
+});
+
+
+
+router.get("/logout", isloggedin, function (req, res) {
+  res.render("shop");
+});
+
+module.exports = router;
